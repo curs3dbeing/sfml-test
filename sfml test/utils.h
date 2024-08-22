bed846aacd40bfad07d4835d1d9d5045aa9914f2
blessedboy14 <92112019+blessedboy14@users.noreply.github.com>
2024-08-22 15:40:36 +0300

    small folder loading improvement

--- sfml test/utils.h
+++ sfml test/utils.h
@@ -13,14 +13,15 @@ float dot(sf::Vector2f left, sf::Vector2f right) {
 std::map<std::string,sf::Texture> loadTextureFolder(std::string path) {
 	std::map<std::string, sf::Texture> texts;
 	for (const auto& entry : std::filesystem::directory_iterator(path)) {
-		sf::Texture newText;
-		if (newText.loadFromFile(entry.path().generic_string())) {
-			texts[entry.path().stem().string()] = newText;
+		if (entry.is_regular_file() && entry.path().extension() == ".png") {
+			sf::Texture newText;
+			if (newText.loadFromFile(entry.path().generic_string())) {
+				texts[entry.path().stem().string()] = newText;
+			}
+			else {
+				throw std::runtime_error("failed to load file from " + path);
+			}
 		}
-		else {
-			throw std::runtime_error("failed to load file from " + path);
-		}
-
 	}
 	return texts;
 }
